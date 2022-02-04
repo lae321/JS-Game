c56057e8dcade712650ac103758272aa7b5c9b13
lae321 <lae321@hotmail.co.uk>
2022-02-04 20:39:17 +0000

    Bug fixes and score counter. hitWall now actually works when wall hit, not when edge of grid entered.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 /// SNAKE \\\
 
+// TODO: On start button click, change start to restart. Can you put all the event listeners in one?
+// TODO: when isDead === true, display you lose :( on the game board, possibly by toggling a class applied to board and updating the innertext
+// TODO: Push to snakeArr when apple is eaten
+
 // Query Selectors
 const board = document.querySelectorAll("div");
 const snake = document.querySelectorAll(".board__snake");
@@ -10,7 +14,6 @@ const scoreCounter = document.querySelector(".info__score");
 // Variables
 let snakeArr = [1, 0]; //snakes head will start in position 1 and will be 2 blocks long
 let snakeHeadPosition = snakeArr[0];
-let snakeStartPosition = 0; // remove snake tail from first box of grid for first movement
 let applePosition = 0;
 let isDead = false;
 let interval = 750;
@@ -19,30 +22,8 @@ let movingDirection = 1; //by default move right from position 1 in board
 let gameStarted = false;
 let score = 0;
 let moveInterval = 0;
-let wallInterval = 0
-
-const newGame = () => {
-  score = 0;
-  snakeArr.forEach((item) => board[item].classList.remove("board__snake"));
-  board[applePosition].classList.remove("board__apple");
-  snakeStartPosition = 0;
-  applePosition = 0;
-  snakeArr = [1, 0];
-  snakeArr.forEach((item) => board[item].classList.add("board__snake"));
-  isDead = false;
-  gameStarted = false;
-  movingDirection = 1;
-  clearInterval(moveInterval);
-  clearInterval(wallInterval)
-  if (gameStarted === false) {
-    moveInterval = setInterval(iterativeMovement, interval);
-    wallInterval = setInterval(hitWall, interval);
-  }
-  gameStarted = true;
-  
-};
-
-startButton.addEventListener("click", newGame);
+let wallInterval = 0;
+let eatAppleInterval = 0;
 
 const move = () => {
   document.onkeydown = (e) => {
@@ -61,6 +42,7 @@ const move = () => {
         break;
     }
   };
+  
 };
 
 startButton.addEventListener("click", move);
@@ -70,19 +52,61 @@ const iterativeMovement = () => {
   board[snakeArr.pop()].classList.remove("board__snake"); // removes tail from current index in board
   snakeArr.unshift(snakeArr[0] + movingDirection); // adds 1 to snake array in the box equivalent to snakehead[index] + direction
   board[snakeArr[0]].classList.add("board__snake");
-  snakeHeadPosition = snakeArr[0]
+  snakeHeadPosition = snakeArr[0]; // update the value of snakeHeadPositon to be the same as snakeArr[0]
 };
 
 const hitWall = () => {
-  console.log(snakeHeadPosition)
   if (
     (snakeHeadPosition % gridSize === 0 && movingDirection === -1) || // snake hits left
     (snakeHeadPosition % gridSize === gridSize - 1 && movingDirection === 1) || // snake hits right
-    (snakeHeadPosition - gridSize < 0 && movingDirection === -gridSize) || //snake hits top
+    (snakeHeadPosition - gridSize < 0 && movingDirection === -gridSize) || // snake hits top
     (snakeHeadPosition + gridSize >= gridSize * gridSize &&
       movingDirection === gridSize) // snake hits bottom
   ) {
     console.log("hit wall");
-    return clearInterval(moveInterval), clearInterval(wallInterval) // cancel movement if above happens
+    return clearInterval(moveInterval), clearInterval(wallInterval); // cancel movement and hitWall function if above happens
+    // isDead = true;
   }
 };
+
+const placeApple = () => {
+  // randomly generate index between 0 and 99
+  let randomIndex = Math.floor(Math.random() * 100);
+  if (board[randomIndex].classList.contains("board__snake") === false) {
+    board[randomIndex].classList.add("board__apple");
+    applePosition = randomIndex;
+  }
+};
+
+const eatApple = () => {
+  if (board[snakeHeadPosition].classList.contains("board__apple")) {
+    board[snakeHeadPosition].classList.remove("board__apple");
+    score++;
+    scoreCounter.innerHTML = `Score: ${score}`;
+    placeApple();
+  }
+};
+
+const newGame = () => {
+  score = 0;
+  snakeArr.forEach((item) => board[item].classList.remove("board__snake"));
+  board[applePosition].classList.remove("board__apple");
+  applePosition = 0;
+  snakeArr = [1, 0];
+  snakeArr.forEach((item) => board[item].classList.add("board__snake"));
+  isDead = false;
+  gameStarted = false;
+  movingDirection = 1;
+  placeApple();
+  clearInterval(moveInterval);
+  clearInterval(wallInterval);
+  clearInterval(eatAppleInterval);
+  wallInterval = setInterval(hitWall, interval); // why did moving this work?
+  if (gameStarted === false) {
+    moveInterval = setInterval(iterativeMovement, interval);
+    eatAppleInterval = setInterval(eatApple, interval);
+  }
+  gameStarted = true;
+};
+
+startButton.addEventListener("click", newGame);
